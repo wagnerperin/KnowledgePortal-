38b1cd4dd9414a7bcfe3de72391f03ad7afcf322
Isaque Rocha <isaquerocha@gmail.com>
2016-10-18 17:19:54 -0200

    Variáveis de logon adicionados a sessão.

--- frontend/views.py
+++ frontend/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.conf import settings
 import requests
@@ -9,28 +9,51 @@ import requests
 def home(request):
     print(request.session.keys())
     context = {
-        "title" : "Portal do Conhecimento"
+        "title": "Portal do Conhecimento"
     }
     return render(request, "index.html", context)
+
 
 def login(request):
     server_ip = settings.CMPAAS_CONF['server_address']
     server_port = settings.CMPAAS_CONF['server_port']
     username = request.POST.get("username", "")
     password = request.POST.get("password", "")
+    access_token = request.POST.get("access_token", "")
+    token_type = request.POST.get("token_type", "")
+    expires_in = request.POST.get("expires_in", "")
+    refresh_token = request.POST.get("refresh_token", "")
+    scope = request.POST.get("scope", "")
 
     # request for authentification
 
     request.session['username'] = username
+    request.session['access_token'] = access_token
+    request.session['expires_in'] = expires_in
+    request.session['refresh_token'] = refresh_token
+    request.session['scope'] = scope
 
     context = {
-        "title" : "Portal do Conhecimento",
-        "username" : username
+        "title": "Portal do Conhecimento",
     }
-    return render(request, "index.html", context)
+    return redirect("/", context)
+
+
+def logout(request):
+
+    # request for authentification
+
+    request.session.clear()
+
+    context = {
+        "title": "Portal do Conhecimento",
+    }
+    # return render(request, "index.html", context
+    return redirect("/", context)
+
 
 def register(request):
     context = {
-        "title" : "Portal do Conhecimento"
+        "title": "Portal do Conhecimento"
     }
     return render(request, "register.html", context)
