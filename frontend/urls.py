6a0c7ca4415207e9385ecd094dfa8a52491aaef9
Isaque Rocha <isaquerocha@gmail.com>
2016-10-18 12:12:49 -0200

    Código todo zoado do wagner.

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home),
     url(r'^login$', views.login),
+    url(r'^logout$', views.logout),
     url(r'^register$', views.register),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
 import requests
+import json
 
 
 # Create your views here.
@@ -18,14 +19,35 @@ def login(request):
     server_port = settings.CMPAAS_CONF['server_port']
     username = request.POST.get("username", "")
     password = request.POST.get("password", "")
+    csrftoken = request.POST.get("csrfmiddlewaretoken", "")
+    url = "http://"+server_ip+":"+server_port+"/o/token/"
+    data = {'client_id':'KAivTIRh7e1ojpEGPh37rV5o3VDwDjeqjsIBMwE2',
+            'client_secret':'mCZ27ZvCCK3lAQv06OpehN8WSwT8YWJAZ8I3XZmrxFygMh8qvPcMCNaK5YbiIP5GKntNIyKDU9cx0GXMH2arOjMLeGpdhMQaMvwxWti7t6nu8HKpcBg4pi8JxcbCQ2Iu',
+            'grant_type':'password',
+            'username':username,
+            'password':password}
+    headers = {'content-type': 'application/x-www-form-urlencoded',
+            'X-CSRFToken': csrftoken}
+    for key, value in requests.post(url, data=json.dumps(data), headers=headers).items():
+        print(key, value)
 
-    # request for authentification
 
     request.session['username'] = username
 
     context = {
         "title" : "Portal do Conhecimento",
         "username" : username
+    }
+    return render(request, "index.html", context)
+
+def logout(request):
+
+    # request for authentification
+
+    request.session.clear()
+
+    context = {
+        "title" : "Portal do Conhecimento",
     }
     return render(request, "index.html", context)
 
