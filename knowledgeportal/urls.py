ef073a763c9f1f37cd631a36a45141b2fa417db6
Ramon <ramondead@gmail.com>
2017-01-05 20:32:37 -0200

    Recriando o projeto do zero.

---
+++ knowledgeportal/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    #url(r'^$', views.home, name="home"),
+    #url(r'^login$', views.login),
+    #url(r'^logout$', views.logout),
+    #url(r'^editor/$', views.editor),
+    #url(r'^profile/$', views.profile, name='profile'),
+    #url(r'^about/$', views.about, name='about'),
+]
