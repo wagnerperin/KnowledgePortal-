a83fde0c0de5c87e17c339284b3a5eb5d10c5585
Ramon <Rahmon@users.noreply.github.com>
2017-03-03 10:38:43 -0300

    Implementação do Login

--- knowledgeportal/urls.py
+++ knowledgeportal/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name="home"),
-    #url(r'^login$', views.login),
-    #url(r'^logout$', views.logout),
+    url(r'^login/$', views.login, name="login"),
+    url(r'^logout$', views.logout),
     url(r'^services/', views.services, name="services"),
     url(r'^editor/$', views.editor),
     url(r'^profile/$', views.profile, name='profile'),
--- knowledgeportal/views.py
+++ knowledgeportal/views.py
@@ -12,20 +12,45 @@ os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 api_url = 'http://127.0.0.1:8000'
 client_id = '2DwBdhnIk9tznlqrVizAVn2SMGjzv7gyS6o9p7ys'
 client_secret = 'ieHPfYtmhtvAqNSuchsJLW9eghBG9tQOexoE9LPmDUboLPDjTmNhiMCpR4nOjg84rMaHpSqa5PqThTms9W0r6A38zfiIHCiaGemU44Ykx0WDQUrzghsdCOBaV22x9mu0'
-username = 'ramon'
-password = '1234432q'
+#username = 'ramon'
+#password = '1234432q'
+
+def verifica_login(request):
+    if request.session.get('access_token'):
+        return True
+    else:
+        return False
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        if not verifica_login(request):
+            oauth = OAuth2Session(client=LegacyApplicationClient(client_id=client_id))
+            token = oauth.fetch_token(token_url=api_url + '/o/token/',
+                username=username, password=password, client_id=client_id,
+                client_secret=client_secret)
+
+            data = json.loads(json.dumps(token))
+
+            request.session['username'] = username
+            request.session['access_token'] = data['access_token']
+            request.session['refresh_token'] = data['refresh_token']
+    
+    return home(request)
+
+def logout(request):
+    try:
+        del request.session['username']
+        del request.session['access_token']
+        del request.session['refresh_token']
+    except KeyError:
+        pass
+    return home(request)
 
 def home(request):
-    oauth = OAuth2Session(client=LegacyApplicationClient(client_id=client_id))
-    token = oauth.fetch_token(token_url=api_url + '/o/token/',
-        username=username, password=password, client_id=client_id,
-        client_secret=client_secret)
-
-    data = json.loads(json.dumps(token))
-
-    request.session['username'] = username
-    request.session['access_token'] = data['access_token']
-    request.session['refresh_token'] = data['refresh_token']
+    #login(request)
 
     context = {
         'title': 'Portal do Conhecimento'
